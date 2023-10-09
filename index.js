d7188d0d89c917826cb6a96385f4437b938e3324
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-10-09 00:55:52 -0500

    figured out why this wasn't working with api

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const cloudinary = require('cloudinary').v2
 require('dotenv').config();
 
+// app.use(cors());
+
 cloudinary.config({
     cloud_name: process.env.CLOUDNAME,
     api_key: process.env.CLOUDAPIKEY,
@@ -10,8 +12,10 @@ cloudinary.config({
 })
 
 app.get('/api/videos', async (req, res) => {
+    console.log("hello?")
     try {
-        const result = await cloudinary.api.resources({ type: 'video' })
+        const result = await cloudinary.api.resources({ resource_type: 'video' })
+        console.log("backend result: " + JSON.stringify(result))
         res.json(result)
     } catch (error) {
         console.error('Error fetching videos from Cloudinary: ', error)
@@ -19,7 +23,7 @@ app.get('/api/videos', async (req, res) => {
     }
 })
 
-const port = 3000
+const port = 3001
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
